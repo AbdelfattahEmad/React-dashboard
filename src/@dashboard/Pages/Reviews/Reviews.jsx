da48cb71fac6160edfcd52280ccd63eb3be9be61
AbdelfattahEmad <abdelfattahemad1299@gmail.com>
2023-11-14 11:56:36 +0200

    14/11/2023

--- src/@dashboard/Pages/Reviews/Reviews.jsx
+++ src/@dashboard/Pages/Reviews/Reviews.jsx
@@ -1,12 +1,69 @@
 import React from 'react'
+import "./Reviews.scss"
 import { useTranslation } from 'react-i18next'
+import abdo from "../../../assets/Images/abdo.jpg"
 
 const Reviews = () => {
   const {t}=useTranslation()
 
   return (
+    <div className='Container_review'>
+      <h2> {t("Reviews")}</h2 >
 
-    <div> {t("Reviews")}</div>
+
+      <div className='inputs'>
+
+        <img src='' alt='Add img' />
+
+      <input  className='input' type='textarea'  placeholder='The Name' /> 
+
+      <input  className='input' type='textarea'  placeholder='Put Your Opinion' /> 
+      <button>Submit</button>
+
+
+
+        </div>  
+
+
+
+<div className='person_info'>
+
+  <img src={abdo} alt='' />
+
+    <h5> mr mohmed </h5>
+    <p> my opinii is this website is very wonderful one </p>
+
+
+</div>
+
+<div className='person_info'>
+
+  <img src={abdo} alt='' />
+
+    <h5> mr mohmed </h5>
+    <p> my opinii is this website is very wonderful one </p>
+
+
+</div>
+
+<div className='person_info'>
+
+  <img src={abdo} alt='' />
+
+    <h5> mr mohmed </h5>
+    <p> my opinii is this website is very wonderful one </p>
+
+
+</div>
+
+
+
+
+
+    
+    
+    </div>
+
   )
 }
 
